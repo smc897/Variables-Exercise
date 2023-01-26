50beb2fc468cc54b529c68ac33ca5c7351382438
ScottMC89 <scottmc592@gmail.com>
2023-01-25 18:18:47 -0600

    outputting vars.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String str = "abc";
+                int intTmp = 5;
+            char c = 'a';
+            bool b = false;
+            double d = 5.6;
+            decimal dc = 6.7m;
+
+            Console.WriteLine("str="+str+",intTmp="+intTmp+",c="+c+",b="+b+",d="+d+",dc="+dc);
         }
     }
 }
